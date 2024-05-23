a8b724c9fa93e4c9ec9fbf3b7a98c29df759caa2
Jakob Krantz <mail@jakobkrantz.se>
2024-05-23 19:57:13 +0200

    v3: Remove minimal watchface to save flash.

--- app/src/applications/sensors_summary/sensors_summary_app.c
+++ app/src/applications/sensors_summary/sensors_summary_app.c
@@ -4,15 +4,13 @@
 #include <zephyr/drivers/sensor.h>
 #include <math.h>
 
-#include "sensors_summary_ui.h"
 #include "sensors/zsw_pressure_sensor.h"
 #include "sensors/zsw_light_sensor.h"
 #include "sensors/zsw_environment_sensor.h"
 #include "managers/zsw_app_manager.h"
 #include "ui/utils/zsw_ui_utils.h"
+#include "ui/sensors_summary_ui.h"
 
-static void sensors_summary_app_start(lv_obj_t *root, lv_group_t *group);
-static void sensors_summary_app_stop(void);
 static void on_close_sensors_summary(void);
 static void on_ref_set(void);
 
@@ -20,27 +18,9 @@ static void timer_callback(lv_timer_t *timer);
 
 ZSW_LV_IMG_DECLARE(move);
 
-static application_t app = {
-    .name = "Sensors",
-    .icon = ZSW_LV_IMG_USE(move),
-    .start_func = sensors_summary_app_start,
-    .stop_func = sensors_summary_app_stop
-};
-
-static lv_timer_t *refresh_timer;
-static float relative_pressure;
-
 static void sensors_summary_app_start(lv_obj_t *root, lv_group_t *group)
 {
-    sensors_summary_ui_show(root, on_close_sensors_summary, on_ref_set);
-
-    // Set inital relative pressure.
-    on_ref_set();
-
-    // Increase ODR since we want high accuracy here.
-    zsw_pressure_sensor_set_odr(BOSCH_BMP581_ODR_160_HZ);
-    refresh_timer = lv_timer_create(timer_callback, CONFIG_DEFAULT_CONFIGURATION_SENSORS_SUMMARY_REFRESH_INTERVAL_MS,
-                                    NULL);
+    Sensors_Summary_UI_Init(root);
 }
 
 static void sensors_summary_app_stop(void)
@@ -49,48 +29,45 @@ static void sensors_summary_app_stop(void)
     // TODO need mechanism so that multiple user can request ODR without
     // breaking for another when changed.
     zsw_pressure_sensor_set_odr(BOSCH_BMP581_ODR_DEFAULT);
-    lv_timer_del(refresh_timer);
-    sensors_summary_ui_remove();
+    //lv_timer_del(refresh_timer);
+    //sensors_summary_ui_remove();
+    SensorsSummary_HomeScreen_Remove();
+}
+
+static bool sensors_summary_app_back(void)
+{
+    if (sensors_summary_screenTemperature != NULL) {
+        Sensors_Summary_UI_ChangeScreen();
+
+        return false;
+    }
+
+    else if (sensors_summary_screenHome != NULL) {
+        return true;
+    }
 }
 
 static double get_relative_height_m(double relative_pressure, double new_pressure, double temperature)
 {
-    return ((powf((relative_pressure / new_pressure), 1.f / 5.257f) - 1.f) * (temperature + 273.15f)) / 0.0065f;
 }
 
 static void timer_callback(lv_timer_t *timer)
 {
-    float temperature = 0.0;
-    float pressure = 0.0;
-    float humidity = 0.0;
-    float light = -1.0;
-    float iaq = -1.0;
-    float co2 = -1.0;
 
-    zsw_environment_sensor_get(&temperature, &humidity, &pressure);
-    zsw_environment_sensor_get_iaq(&iaq);
-    zsw_environment_sensor_get_co2(&co2);
-    zsw_pressure_sensor_get_pressure(&pressure);
-    zsw_light_sensor_get_light(&light);
-
-    sensors_summary_ui_set_pressure(pressure);
-    sensors_summary_ui_set_temp(temperature);
-    sensors_summary_ui_set_humidity(humidity);
-    sensors_summary_ui_set_iaq(iaq);
-    sensors_summary_ui_set_co2(co2);
-    sensors_summary_ui_set_light(light);
-    sensors_summary_ui_set_rel_height(get_relative_height_m(relative_pressure, pressure, temperature));
-}
-
-static void on_close_sensors_summary(void)
-{
-    zsw_app_manager_app_close_request(&app);
 }
 
 static void on_ref_set(void)
 {
-    zsw_pressure_sensor_get_pressure(&relative_pressure);
+    //zsw_pressure_sensor_get_pressure(&relative_pressure);
 }
+
+static application_t app = {
+    .name = "Sensors",
+    .icon = ZSW_LV_IMG_USE(move),
+    .start_func = sensors_summary_app_start,
+    .stop_func = sensors_summary_app_stop,
+    .back_func = sensors_summary_app_back
+};
 
 static int sensors_summary_app_add(void)
 {
