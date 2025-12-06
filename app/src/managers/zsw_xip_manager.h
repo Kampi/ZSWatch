5130f791edd916df1f90e0f0a8d0f979365ffc40
Daniel Kampert <DanielKampert@kampis-elektroecke.de>
2025-12-06 12:03:01 +0100

    Fix CI/CD issues

--- app/src/managers/zsw_xip_manager.h
+++ app/src/managers/zsw_xip_manager.h
@@ -20,10 +20,10 @@
 int _zsw_xip_enable(const char *requester);
 int _zsw_xip_disable(const char *requester);
 
-#ifdef CONFIG_NORDIC_QSPI_NOR_XIP
+#ifdef CONFIG_ZSW_XIP
 #define zsw_xip_enable() _zsw_xip_enable(__FUNCTION__)
 #define zsw_xip_disable() _zsw_xip_disable(__FUNCTION__)
 #else
-#define zsw_xip_enable() (0)
-#define zsw_xip_disable() (0)
+#define zsw_xip_enable()
+#define zsw_xip_disable()
 #endif
